39296c2f30ea6bd0684a443bea98285d4c5d7938
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-10-05 23:20:03 +0800

    Delete output directory

--- output/randint.py
+++
@@ -1,6 +0,0 @@
-def randint(l, u):
-    c = ''
-    while not bool(len(str(c))):
-        with open('C:\\Users\\jerom\\Documents\\NoModule\\number.txt', 'r') as file:
-            c = file.read()
-    return int(u*float(c)+l)
